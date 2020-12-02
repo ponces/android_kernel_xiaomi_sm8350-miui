f602065536f4599fcbe560d173d92a1f130d52d7
Stanislav Fomichev <sdf@google.com>
2020-12-02 09:25:15 -0800

    UPSTREAM: bpf: Allow bpf_{s,g}etsockopt from cgroup bind{4,6} hooks
    I have to now lock/unlock socket for the bind hook execution.
    That shouldn't cause any overhead because the socket is unbound
    and shouldn't receive any traffic.
    
    Change-Id: Id72e869eae0c66d6935fe1f965799ace84cc8b93
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrey Ignatov <rdna@fb.com>
    Link: https://lore.kernel.org/bpf/20201202172516.3483656-3-sdf@google.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -7063,6 +7063,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_sk_storage_delete_proto;
 	case BPF_FUNC_setsockopt:
 		switch (prog->expected_attach_type) {
+		case BPF_CGROUP_INET4_BIND:
+		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
 			return &bpf_sock_addr_setsockopt_proto;
@@ -7071,6 +7073,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		}
 	case BPF_FUNC_getsockopt:
 		switch (prog->expected_attach_type) {
+		case BPF_CGROUP_INET4_BIND:
+		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
 			return &bpf_sock_addr_getsockopt_proto;
