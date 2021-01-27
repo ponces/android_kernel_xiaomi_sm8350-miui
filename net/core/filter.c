59e94a1a7d0e1b3e11e6fa3209c3cad6995100bb
Stanislav Fomichev <sdf@google.com>
2021-01-27 15:28:50 -0800

    UPSTREAM: bpf: Enable bpf_{g,s}etsockopt in BPF_CGROUP_UDP{4,6}_SENDMSG
    Can be used to query/modify socket state for unconnected UDP sendmsg.
    Those hooks run as BPF_CGROUP_RUN_SA_PROG_LOCK and operate on
    a locked socket.
    
    Change-Id: Ie8b8cbc784c6d81a8d344a1e76a6fb8019276bf5
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20210127232853.3753823-2-sdf@google.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -7067,6 +7067,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
+		case BPF_CGROUP_UDP4_SENDMSG:
+		case BPF_CGROUP_UDP6_SENDMSG:
 			return &bpf_sock_addr_setsockopt_proto;
 		default:
 			return NULL;
@@ -7077,6 +7079,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
+		case BPF_CGROUP_UDP4_SENDMSG:
+		case BPF_CGROUP_UDP6_SENDMSG:
 			return &bpf_sock_addr_getsockopt_proto;
 		default:
 			return NULL;
