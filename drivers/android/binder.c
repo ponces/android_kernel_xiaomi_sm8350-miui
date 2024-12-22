d38954932f8260fd7c2a0721908693f1b6a3da93
ralph950412 <ralph950412@gmail.com>
2024-12-23 02:14:44 +0800

    Revert "HACK: binder: Disable freeze related ioctls"
    This reverts commit ba4317f545b96f7b11d2da98932dee8cc5678d23.
    
    Change-Id: I492e117048137b96383723cff555e822e40ed858

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5804,6 +5804,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5882,6 +5883,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
