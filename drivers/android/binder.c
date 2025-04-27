cf74e41fe1d44e3eb23986e9dddefb5999b4a215
EndCredits <endcredits@crepuscular-aosp.icu>
2025-04-27 22:47:28 +0800

    ANDROID: binder: Activate binder freeze ioctl
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5523,7 +5523,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5602,7 +5601,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
