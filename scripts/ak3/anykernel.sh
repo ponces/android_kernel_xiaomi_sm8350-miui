044119e1af0482c7d537e3a7a1a9986f2486e53d
Alberto Ponces <AlbertoPonces@criticalmanufacturing.com>
2026-02-02 11:48:21 +0000

    Revert "build.sh: Support to build for multiple device"
    This reverts commit dabf2f98f010bb657f76cdb3ba985b812799ad03.

--- scripts/ak3/anykernel.sh
+++ scripts/ak3/anykernel.sh
@@ -4,13 +4,13 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel for DEVICE_PLACEHOLDER by EndCredits @ Github
+kernel.string=Kernel for star by EndCredits @ Github
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=DEVICE_PLACEHOLDER
+device.name1=star
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
