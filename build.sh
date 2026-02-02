b59f951217e0ec3f2de90d1f4df869924f1fc285
Alberto Ponces <AlbertoPonces@criticalmanufacturing.com>
2026-02-02 11:12:03 +0000

    Revert "build.sh: Fix GCC build"
    This reverts commit c85e6f900cd3e6688f4a98a90475018d22af5ffc.

--- build.sh
+++ build.sh
@@ -26,7 +26,8 @@ CC_ADDITIONAL_FLAGS="LLVM_IAS=1 LLVM=1";
 TARGET_OUT="../out";
 
 WITH_GCC=0;
-CC_GCC_ADDITIONAL_FLAGS="";
+CC_GCC=gcc;
+CC_GCC_ADDITIONAL_FLAGS="LLVM_IAS=0 LLVM=0 LD=ld";
 
 TARGET_KERNEL_FILE=arch/arm64/boot/Image;
 TARGET_KERNEL_DTB=arch/arm64/boot/dtb;
@@ -159,6 +160,7 @@ main(){
         echo "Building with system GCC"
         WITH_GCC=1
         FINAL_KERNEL_BUILD_PARA="ARCH=$TARGET_ARCH \
+                             CC=$CC_GCC \
                              CROSS_COMPILE=$TARGET_CROSS_COMPILE \
                              CROSS_COMPILE_COMPAT=$TARGET_CROSS_COMPILE_COMPAT \
                              $CC_GCC_ADDITIONAL_FLAGS \
