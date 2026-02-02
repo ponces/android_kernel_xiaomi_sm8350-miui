56d78bfd17c1e3a6da17fdbffda9a323415aeb17
Alberto Ponces <AlbertoPonces@criticalmanufacturing.com>
2026-02-02 11:48:21 +0000

    Revert "build.sh: Support to build for multiple device"
    This reverts commit dabf2f98f010bb657f76cdb3ba985b812799ad03.

--- build.sh
+++ build.sh
@@ -14,14 +14,14 @@
 
 set -e
 
-TARGET_ARCH=arm64;
-TARGET_CC=clang;
-TRAGET_CLANG_TRIPLE=aarch64-linux-gnu-;
-TARGET_CROSS_COMPILE=aarch64-linux-gnu-;
-TARGET_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-;
-THREAD=$(nproc --all);
-CC_ADDITIONAL_FLAGS="LLVM_IAS=1 LLVM=1";
-TARGET_OUT="../out";
+TARGET_ARCH=arm64
+TARGET_CC=clang
+TRAGET_CLANG_TRIPLE=aarch64-linux-gnu-
+TARGET_CROSS_COMPILE=aarch64-linux-gnu-
+TARGET_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
+THREAD=$(nproc --all)
+CC_ADDITIONAL_FLAGS="LLVM_IAS=1 LLVM=1"
+TARGET_OUT="../out"
 
 FINAL_KERNEL_BUILD_PARA="ARCH=$TARGET_ARCH \
                          CC=$TARGET_CC \
@@ -29,84 +29,78 @@ FINAL_KERNEL_BUILD_PARA="ARCH=$TARGET_ARCH \
                          CROSS_COMPILE_COMPAT=$TARGET_CROSS_COMPILE_COMPAT \
                          CLANG_TRIPLE=$TARGET_CLANG_TRIPLE \
                          $CC_ADDITIONAL_FLAGS \
-                         -j$THREAD
-                         O=$TARGET_OUT";
+                         -j$THREAD \
+                         O=$TARGET_OUT"
 
-TARGET_KERNEL_FILE=arch/arm64/boot/Image;
-TARGET_KERNEL_DTB=arch/arm64/boot/dtb;
+TARGET_KERNEL_FILE=arch/arm64/boot/Image
+TARGET_KERNEL_DTB=arch/arm64/boot/dtb
 TARGET_KERNEL_DTBO=arch/arm64/boot/dtbo.img
-TARGET_KERNEL_NAME=Hana-kernel;
+TARGET_KERNEL_NAME=Hana-kernel
 TARGET_KERNEL_MOD_VERSION=$(make kernelversion)
 
 ANYKERNEL_PATH=anykernel
 
 DEFCONFIG_PATH=arch/arm64/configs
+DEFCONFIG_NAME="vendor/star_defconfig"
 
-START_SEC=$(date +%s);
-CURRENT_DATE=$(date '+%Y%m%d');
+START_SEC=$(date +%s)
+CURRENT_DATE=$(date '+%Y%m%d')
 
-link_all_dtb_files(){
-    find $TARGET_OUT/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > $TARGET_OUT/arch/arm64/boot/dtb;
+link_all_dtb_files() {
+    find $TARGET_OUT/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > $TARGET_OUT/arch/arm64/boot/dtb
 }
 
-make_defconfig(){
-    echo "------------------------------";
-    echo " Building Kernel Defconfig..";
-    echo "------------------------------";
+make_defconfig() {
+    echo "------------------------------"
+    echo " Building Kernel Defconfig.."
+    echo "------------------------------"
 
-    make $FINAL_KERNEL_BUILD_PARA $DEFCONFIG_NAME;
+    make $FINAL_KERNEL_BUILD_PARA $DEFCONFIG_NAME
 }
 
-build_kernel(){
-    echo "------------------------------";
-    echo " Building Kernel ...........";
-    echo "------------------------------";
+build_kernel() {
+    echo "------------------------------"
+    echo " Building Kernel ..........."
+    echo "------------------------------"
 
-    make $FINAL_KERNEL_BUILD_PARA;
-    END_SEC=$(date +%s);
-    COST_SEC=$[ $END_SEC-$START_SEC ];
+    make $FINAL_KERNEL_BUILD_PARA
+    END_SEC=$(date +%s)
+    COST_SEC=$[ $END_SEC-$START_SEC ]
     echo "Kernel Build Costed $(($COST_SEC/60))min $(($COST_SEC%60))s"
 
 }
 
-generate_flashable(){
-    echo "------------------------------";
-    echo " Generating Flashable Kernel";
-    echo "------------------------------";
+generate_flashable() {
+    echo "------------------------------"
+    echo " Generating Flashable Kernel"
+    echo "------------------------------"
 
-    FLASHABLE_KERNEL_NAME="${TARGET_KERNEL_NAME}-${TARGET_DEVICE}-${CURRENT_DATE}-${TARGET_KERNEL_MOD_VERSION}"
+    echo " Removing old package file "
+    rm -rf $TARGET_OUT/$ANYKERNEL_PATH
 
-    echo " Removing old package file ";
-    rm -rf $TARGET_OUT/$ANYKERNEL_PATH;
-
-    echo " Getting AnyKernel ";
+    echo " Getting AnyKernel "
     cp -r ./scripts/ak3 $TARGET_OUT/$ANYKERNEL_PATH
 
-    cd $TARGET_OUT;
+    cd $TARGET_OUT
 
-    echo " Setting device info ";
-    sed -i "s/DEVICE_PLACEHOLDER/${TARGET_DEVICE}/g" $ANYKERNEL_PATH/anykernel.sh
+    echo " Copying Kernel File "
+    cp -r $TARGET_KERNEL_FILE $ANYKERNEL_PATH/
 
-    echo " Copying Kernel File ";
-    cp -r $TARGET_KERNEL_FILE $ANYKERNEL_PATH/;
-    # cp -r $TARGET_KERNEL_DTB $ANYKERNEL_PATH/;
-    # cp -r $TARGET_KERNEL_DTBO $ANYKERNEL_PATH/;
+    echo " Packaging flashable Kernel "
+    cd $ANYKERNEL_PATH
+    zip -q -r $TARGET_KERNEL_NAME-$CURRENT_DATE-$TARGET_KERNEL_MOD_VERSION.zip *
 
-    echo " Packaging flashable Kernel ";
-    cd $ANYKERNEL_PATH;
-    zip -q -r ${FLASHABLE_KERNEL_NAME}.zip *;
-
-    echo " Target File:  ../out/anykernel/${FLASHABLE_KERNEL_NAME}.zip ";
+    echo " Target File: $TARGET_OUT/$ANYKERNEL_PATH/$TARGET_KERNEL_NAME-$CURRENT_DATE-$TARGET_KERNEL_MOD_VERSION.zip "
 }
 
-save_defconfig(){
-    echo "------------------------------";
-    echo " Saving kernel config ........";
-    echo "------------------------------";
+save_defconfig() {
+    echo "------------------------------"
+    echo " Saving kernel config ........"
+    echo "------------------------------"
 
-    make $FINAL_KERNEL_BUILD_PARA savedefconfig;
-    END_SEC=$(date +%s);
-    COST_SEC=$[ $END_SEC-$START_SEC ];
+    make $FINAL_KERNEL_BUILD_PARA savedefconfig
+    END_SEC=$(date +%s)
+    COST_SEC=$[ $END_SEC-$START_SEC ]
     echo "Finished. Kernel config saved to $TARGET_OUT/defconfig"
     echo "Moving kernel defconfig to source tree"
     mv $TARGET_OUT/defconfig $DEFCONFIG_PATH/$DEFCONFIG_NAME
@@ -114,74 +108,57 @@ save_defconfig(){
 
 }
 
-clean(){
+clean() {
     echo "Clean source tree and build files..."
-    make mrproper -j$THREAD;
-    make clean -j$THREAD;
-    rm -rf $TARGET_OUT;
-    git checkout HEAD drivers/input/touchscreen;
+    make mrproper -j$THREAD
+    make clean -j$THREAD
+    rm -rf $TARGET_OUT
+    git checkout HEAD drivers/input/touchscreen
 }
 
 display_help() {
-        echo "build.sh: A very simple Kernel build helper"
-        echo "usage: build.sh <build option> <device>"
-        echo
-        echo "Build options:"
-        echo "    all             Perform a build without cleaning."
-        echo "    cleanbuild      Clean the source tree and build files then perform a all build."
-        echo
-        echo "    flashable        Only generate the flashable zip file. Don't use it before you have built once."
-        echo "    savedefconfig    Save the defconfig file to source tree."
-        echo "    kernelonly      Only build kernel image"
-        echo "    defconfig        Only build kernel defconfig"
-        echo "    help ( -h )     Print help information."
-        echo
+    echo "build.sh: A very simple Kernel build helper"
+    echo "usage: build.sh <build option>"
+    echo
+    echo "Build options:"
+    echo "    all             Perform a build without cleaning."
+    echo "    cleanbuild      Clean the source tree and build files then perform a all build."
+    echo
+    echo "    flashable        Only generate the flashable zip file. Don't use it before you have built once."
+    echo "    savedefconfig    Save the defconfig file to source tree."
+    echo "    kernelonly      Only build kernel image"
+    echo "    defconfig        Only build kernel defconfig"
+    echo "    help ( -h )     Print help information."
+    echo
 }
 
-main(){
-    if [ $2 ]; then
-        echo "Building for ${2}"
-    else
-        echo "Missing device. Please check usage"
-        echo
+main() {
+    if [ $1 == "help" -o $1 == "-h" ]; then
         display_help
-        exit -1
-    fi
-    TARGET_DEVICE=$2
-    DEFCONFIG_NAME="vendor/${TARGET_DEVICE}_defconfig";
-    if [ $1 == "help" -o $1 == "-h" ]
-    then
-        display_help
-    elif [ $1 == "savedefconfig" ]
-    then
-        save_defconfig;
-    elif [ $1 == "cleanbuild" ]
-    then
-        clean;
-        make_defconfig;
-        build_kernel;
-        link_all_dtb_files;
-        generate_flashable;
-    elif [ $1 == "flashable" ]
-    then
-        link_all_dtb_files
-        generate_flashable;
-    elif [ $1 == "kernelonly" ]
-    then
-        make_defconfig
-        build_kernel
-    elif [ $1 == "all" ]
-    then
+    elif [ $1 == "savedefconfig" ]; then
+        save_defconfig
+    elif [ $1 == "cleanbuild" ]; then
+        clean
         make_defconfig
         build_kernel
         link_all_dtb_files
         generate_flashable
-    elif [ $1 == "defconfig" ]
-    then
-        make_defconfig;
+    elif [ $1 == "flashable" ]; then
+        link_all_dtb_files
+        generate_flashable
+    elif [ $1 == "kernelonly" ]; then
+        make_defconfig
+        build_kernel
+    elif [ $1 == "all" ]; then
+        make_defconfig
+        build_kernel
+        link_all_dtb_files
+        generate_flashable
+    elif [ $1 == "defconfig" ]; then
+        make_defconfig
     else
         display_help
     fi
 }
 
-main "$1" "$2";
+main "$1"
