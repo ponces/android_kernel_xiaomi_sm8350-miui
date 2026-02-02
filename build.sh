26d57e14f42ef3b4d9304356dfa55a65f54629be
Alberto Ponces <AlbertoPonces@criticalmanufacturing.com>
2026-02-02 11:49:38 +0000

    Revert "build.sh: Correct defconfig name"
    This reverts commit 54634d9993dc6acaedb5cb400049d99300d8dc13.

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ TARGET_KERNEL_MOD_VERSION=$(make kernelversion)
 ANYKERNEL_PATH=anykernel
 
 DEFCONFIG_PATH=arch/arm64/configs
-DEFCONFIG_NAME="vendor/star_defconfig"
+DEFCONFIG_NAME="star_defconfig"
 
 START_SEC=$(date +%s)
 CURRENT_DATE=$(date '+%Y%m%d')
