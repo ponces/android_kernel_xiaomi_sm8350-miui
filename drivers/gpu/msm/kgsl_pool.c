8641d26265c6a5558b42270c5d69d337ef4e60cb
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2026-07-10 13:50:16 +0100

    Reapply "msm: kgsl: Add tracepoints for the pools"
    This reverts commit 90543b3466dd3eba06876f389d776b0fc2c0d42f.

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -11,6 +11,7 @@
 #include "kgsl_device.h"
 #include "kgsl_pool.h"
 #include "kgsl_sharedmem.h"
+#include "kgsl_trace.h"
 
 /**
  * struct kgsl_page_pool - Structure to hold information for the pool
@@ -107,6 +108,7 @@ _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 	llist_add((struct llist_node *)&p->lru, &pool->page_list);
 	atomic_inc(&pool->page_count);
 
+	trace_kgsl_pool_add_page(pool->pool_order, pool->page_count);
 	mod_node_page_state(page_pgdat(p),  NR_KERNEL_MISC_RECLAIMABLE,
 				(1 << pool->pool_order));
 }
@@ -127,6 +129,7 @@ _kgsl_pool_get_page(struct kgsl_page_pool *pool)
 
 	atomic_dec(&pool->page_count);
 	p = container_of((struct list_head *)node, typeof(*p), lru);
+	trace_kgsl_pool_get_page(pool->pool_order, pool->page_count);
 	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
 				-(1 << pool->pool_order));
 	return p;
@@ -196,6 +199,7 @@ _kgsl_pool_shrink(struct kgsl_page_pool *pool,
 
 		__free_pages(page, pool->pool_order);
 		pcount += (1 << pool->pool_order);
+		trace_kgsl_pool_free_page(pool->pool_order);
 	}
 
 	return pcount;
@@ -370,10 +374,11 @@ static int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			if (order > 0) {
 				size = PAGE_SIZE << --order;
 				goto eagain;
-
 			} else
 				return -ENOMEM;
 		}
+
+		trace_kgsl_pool_alloc_page_system(order);
 		goto done;
 	}
 
@@ -393,6 +398,7 @@ static int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			page = alloc_pages(gfp_mask, order);
 			if (page == NULL)
 				return -ENOMEM;
+			trace_kgsl_pool_alloc_page_system(order);
 			goto done;
 		}
 	}
@@ -415,6 +421,8 @@ static int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			} else
 				return -ENOMEM;
 		}
+
+		trace_kgsl_pool_alloc_page_system(order);
 	}
 
 done:
@@ -434,6 +442,7 @@ done:
 	return pcount;
 
 eagain:
+	trace_kgsl_pool_try_page_lower(get_order(*page_size));
 	*page_size = kgsl_get_page_size(size, ilog2(size));
 	*align = ilog2(*page_size);
 	return -EAGAIN;
@@ -517,6 +526,7 @@ static void kgsl_pool_free_page(struct page *page)
 
 	/* Give back to system as not added to pool */
 	__free_pages(page, page_order);
+	trace_kgsl_pool_free_page(page_order);
 }
 
 /* Functions for the shrinker */
